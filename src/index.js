eaf8fad67c5ce5484cd0e518e7981290fc9e2c6b
MrWang609 <609854401@qq.com>
2020-04-01 21:05:10 +0800

    hoc

--- src/index.js
+++ src/index.js
@@ -1,24 +1,2 @@
 import React,{useState,useEffect} from 'react'
 import {render} from 'react-dom'
-
-const Counter=()=>{
-    //console.log(useState(0))
-    const [count,setCount]=useState(100)
-    const increment=()=>setCount(count+1)
-    const decrement=()=>setCount(count-1)
-    useEffect(()=>{
-        document.title=`计数：${count}`
-    })
-    return(
-        <div>
-            <button onClick={decrement}>-</button>
-            <span>{count}</span>
-            <button onClick={increment}>+</button>
-        </div>
-    )
-}
-
-render(
-    <Counter/>,
-    document.getElementById('root')
-)
