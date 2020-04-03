7056720ac62ed1bdf62dd88b6a05575c1a8cc5da
MrWang609 <609854401@qq.com>
2020-04-03 12:20:34 +0800

    redux-g

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import React from 'react'
 import {render} from 'react-dom'
+import App from './App'
 
+import store from './store'
+window.store=store
+
+render(
+    <App store={store}/>,
+    document.querySelector('#root')
+)
